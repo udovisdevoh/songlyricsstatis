913760f82403a1954effbac8de26f77e7a441593
glacasse <glacasse@3da01d16-e9ca-11de-9c2a-cb0f5ba72f9d>
2009-12-27 23:09:51 +0000

    Continued stub for phonetic table repairer

--- phoneticTable/phoneticTable/HomophoneGroup.cs
+++ phoneticTable/phoneticTable/HomophoneGroup.cs
@@ -5,7 +5,69 @@ using System.Text;
 
 namespace LyricThemeClassifier
 {
-    class HomophoneGroup
+    class HomophoneGroup : IEnumerable<string>
     {
+        #region Fields
+        private string phoneticValue;
+
+        private HashSet<string> wordVariantList;
+        #endregion
+
+        #region Constructor
+        public HomophoneGroup(string phoneticValue)
+        {
+            this.phoneticValue = phoneticValue;
+            wordVariantList = new HashSet<string>();
+        }
+        #endregion
+
+        #region Public Methods
+        public bool Remove(string wordVariant)
+        {
+            return wordVariantList.Remove(wordVariant);
+        }
+
+        public bool Add(string wordVariant)
+        {
+            return wordVariantList.Add(wordVariant);
+        }
+        #endregion
+
+        #region Properties
+        public string PhoneticValue
+        {
+            get { return phoneticValue; }
+        }
+
+        public string ShortestVariant
+        {
+            get
+            {
+                string shortestVariant = null;
+
+                foreach (string wordVariant in wordVariantList)
+                {
+                    if (shortestVariant == null || wordVariant.Length < shortestVariant.Length)
+                    {
+                        shortestVariant = wordVariant;
+                    }
+                }
+
+                return shortestVariant;
+            }
+        }
+        #endregion
+
+        #region IEnumerable<string> Members
+        public IEnumerator<string> GetEnumerator()
+        {
+            return wordVariantList.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return wordVariantList.GetEnumerator();
+        }
+        #endregion
     }
 }
