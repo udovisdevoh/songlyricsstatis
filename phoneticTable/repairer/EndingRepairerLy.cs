754145173f9f6a4bec95f050c07be8f5e94bb82a
glacasse <glacasse@3da01d16-e9ca-11de-9c2a-cb0f5ba72f9d>
2009-12-29 20:49:02 +0000

    Phonetic table repairer upgrade

---
+++ phoneticTable/repairer/EndingRepairerLy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LyricThemeClassifier
+{
+    class EndingRepairerLy : EndingRepairer
+    {
+        public override bool IsMatchEndingType(string wordVariant, HomophoneGroup homophoneGroup)
+        {
+            return wordVariant == homophoneGroup.ShortestVariant + "ly";
+        }
+
+        public override string BuildPhoneticEnding(string phoneticValue)
+        {
+            phoneticValue = phoneticValue.Replace("[lprime]", "");
+            phoneticValue = phoneticValue.Replace("[prime]", "");
+
+            while (phoneticValue.Contains("  "))
+                phoneticValue = phoneticValue.Replace("  ", " ");
+
+            phoneticValue = phoneticValue.Trim();
+
+            string phoneticEnding;
+
+            if (phoneticValue.EndsWith("[lreg]"))
+            {
+                phoneticEnding = "[emacr]";
+            }
+            else
+            {
+                phoneticEnding = "[lreg] [emacr]";
+            }
+
+            return phoneticEnding;
+        }
+    }
+}
