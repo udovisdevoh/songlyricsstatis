5ee0e55d3a8cd12f6f8dc73fb75fd888b0d6e4c5
glacasse <glacasse@3da01d16-e9ca-11de-9c2a-cb0f5ba72f9d>
2009-12-29 22:03:06 +0000

    Phonetic table repairer upgrade

---
+++ trunk/phoneticTable/repairer/EndingRepairerYtoIes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LyricThemeClassifier
+{
+    class EndingRepairerYtoIes : EndingRepairer
+    {
+        public override bool IsMatchEndingType(string wordVariant, string shortHomophone)
+        {
+            if (shortHomophone.EndsWith("y"))
+                if (wordVariant.EndsWith("ies"))
+                    return true;
+
+            return false;
+        }
+
+        public override string BuildPhoneticEnding(string phoneticValue)
+        {
+            return "[zreg] [lprime]";
+        }
+    }
+}
